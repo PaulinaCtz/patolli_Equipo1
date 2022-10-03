ef812aa6b18f5f77eae015ad1c62cf248086d1fd
Alfredo Valenzuela <jaime.valenzuela228249@potros.itson.edu.mx>
2022-10-03 01:07:53 -0700

    no message

--- interfaces/src/interfaces/Juego.java
+++ interfaces/src/interfaces/Juego.java
@@ -5,7 +5,7 @@ import java.awt.Graphics;
 import java.awt.Polygon;
 
 public class Juego {
-
+    
     public static void tablero(Graphics g) {
         //Cuadros Amarillos
         
@@ -147,45 +147,6 @@ public class Juego {
 
         //Redondeo de Aspas
         g.setColor(Color.black);
-
-//                int xValues3[] = {300,468,548,380};
-//        int yValues3[] = {380,548,468,300};
-//        Polygon polygon3 = new Polygon(xValues3, yValues3, 6);
-//        g.drawPolygon(polygon3);
-//        
-//        int xValues[] = {20, 40, 50, 30, 20, 15};
-//        int yValues[] = {50, 50, 60, 80, 80, 60};
-//        Polygon polygon1 = new Polygon(xValues, yValues, 6);
-//        g.drawPolygon(polygon1);
-//        int xValues2[] = {70, 90, 100, 80, 70, 65, 60};
-//        int yValues2[] = {100, 100, 110, 110, 130, 110, 90};
-//
-//        g.drawPolyline(xValues2, yValues2, 7);
-//        int xValues3[] = {120, 140, 150, 190};
-//        int yValues3[] = {40, 70, 80, 60};
-//        g.fillPolygon(xValues3, yValues3, 4);
-//        Polygon polygon2 = new Polygon();
-//        polygon2.addPoint(165, 135);
-//        polygon2.addPoint(175, 150);
-//        polygon2.addPoint(270, 200);
-//        polygon2.addPoint(200, 220);
-//        polygon2.addPoint(130, 180);
-//        g.fillPolygon(polygon2);
-// g.setColor(Color.red);
-//        g.drawRect(35, 35, 70, 105);
-//        g.drawOval(52, 87, 35, 35);
-//        g.setColor(Color.green);
-//        g.drawRect(140, 35, 70, 105);
-//        g.drawOval(157, 87, 35, 35);
-//        g.setColor(Color.yellow);
-//        g.drawRect(245, 35, 70,105 );
-//        g.drawOval(262, 87, 35, 35);
-//        g.setColor(Color.blue);
-//        g.drawRect(70, 175, 70, 105);
-//        g.drawOval(87, 217, 35, 35);
-//        g.setColor(Color.pink);
-//        g.drawRect(210, 175, 70, 105);
-//        g.drawOval(227, 227, 35, 35);
     }
 
     public static void dados(Graphics g) {
